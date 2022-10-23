c5438d68407fc254d86d9aa8753bcd62162f33a6
Henrik Thue Oelgaard <DeveloperOelgaard@gmail.com>
2022-10-23 20:21:44 +0200

    Removed clottering Debug.log writes

--- Assets/Scripts/SFX/AnimationNone.cs
+++ Assets/Scripts/SFX/AnimationNone.cs
@@ -4,11 +4,11 @@ public class AnimationNone: IAnimator
 {
     public void StartAnimation()
     {
-        Debug.LogWarning("No Animation selected. Should this be fired?");
+        //Debug.LogWarning("No Animation selected. Should this be fired?");
     }
 
     public void StopAnimation()
     {
-        Debug.LogWarning("No Animation selected. Should this be fired?");
+        //Debug.LogWarning("No Animation selected. Should this be fired?");
     }
 }
