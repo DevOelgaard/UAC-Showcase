0e03c8d2cb5d411a78978a715ce4cd6cb09fc94c
Henrik Thue Oelgaard <DeveloperOelgaard@gmail.com>
2022-10-25 09:52:26 +0200

    Fixed exampel error with using AiContext

---
+++ Assets/Scripts/UAC/Actions/AttackAllTargets.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class AttackAllTargets: AgentAction
+{
+        public override void OnStart(IAiContext context)
+        {
+                var attacker = UacHelper.GetUnitFromAiContext(context);
+                var validTargets = context.GetContext<List<Unit>>(ContextKeys.ValidTargets,this);
+
+                foreach (var validTarget in validTargets)
+                {
+                        InteractionHandler.Attack(attacker,validTarget);
+                }
+        }
+}
---
+++ Assets/Scripts/UAC/Decisions/EvaluateAllTargetsSaveValidTargets.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class EvaluateAllTargetsSaveValidTargets: Decision
+{
+    private GameMap gameMap;
+
+    private GameMap GameMap
+    {
+        get
+        {
+            if (gameMap == null)
+            {
+                gameMap = GameObject.Find("GameMap").GetComponent<GameMap>();
+            }
+
+            return gameMap;
+        }
+    }
+    public EvaluateAllTargetsSaveValidTargets()
+    {
+        AddParameter("Evaluate Defenders", true);
+        AddParameter("Evaluate Attackers", false);
+        Name = "Evaluate All Targets";
+        Description = "Evaluates all targets and saves the valid ones. Returns the highest utility";
+        HelpText = "Uses AiContext";
+    }
+
+    protected override float CalculateUtility(IAiContext context)
+    {
+        var unitContext = context as UnitAiContext;
+        var targets = new List<Unit>();
+
+        var evaluateDefenders = ParameterContainer.GetParamBool("Evaluate Defenders");
+        var evaluateAttackers = ParameterContainer.GetParamBool("Evaluate Attackers");
+
+        // Adding defensive structures if parameter is set
+        if (evaluateDefenders.Value)
+        {
+            foreach (var spawnPoint in GameMap.structureSpawnPoints)
+            {
+                targets.AddRange(spawnPoint.UnitsSpawned);
+            }
+        }
+
+        // Adding attackers if parameter is set
+        if (evaluateAttackers.Value)
+        {
+            foreach (var spawnPoint in GameMap.unitSpawnPoints)
+            {
+                targets.AddRange(spawnPoint.UnitsSpawned);
+            }
+        }
+        
+        var highestUtility = float.MinValue;
+        List<Unit> validTargets = new List<Unit>();
+
+        // Calculates Utility for each potential target;
+        foreach (var target in targets)
+        {
+            // Setting current target in context, for it to be evaluated
+            unitContext.Target = target;
+            var utility = context.UtilityScorer.CalculateUtility(Considerations.Values, unitContext);
+            if (utility > 0)
+            {
+                if (utility > highestUtility)
+                {
+                    highestUtility = utility;
+                }
+                validTargets.Add(target);
+            }
+        }
+        
+        // Setting the highest valued target in unitContext, for it to be used by AgentActions if decisions is selected
+        unitContext.SetContext(ContextKeys.ValidTargets,validTargets,this);
+        
+        return highestUtility > 0 ? highestUtility : 0;
+    }
+}
